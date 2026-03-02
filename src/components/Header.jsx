71e1d198560957e57d92590b85d053eb991913ac
Davit Nakhutsrishvili <dtnakhutsrishvili@gmail.com>
2026-03-02 23:34:46 +0400

    feat:add navbar with resposnsive design

---
+++ src/components/Header.jsx
@@ -0,0 +1,90 @@
+
+import { useState } from 'react';
+import logo from '../assets/icons/logo.png'
+import vectors from '../assets/icons/vectors.png'
+import check from '../assets/icons/check.png'
+import text from '../assets/icons/text.png'
+import mic from '../assets/icons/mic.png'
+import wave from '../assets/icons/wave.png'
+import docs from '../assets/icons/docs.png'
+import styles from './header.module.css'
+
+function Header() {
+ const [isOpen, setIsOpen] = useState(false);
+   const userName = "David";
+
+
+  const toggleMenu = () => {
+    setIsOpen(prev => !prev);
+  };
+
+  return (
+    <header>
+        <div className={styles.header}>
+      <div className={styles.logo}>
+        <img src={logo} alt='logo' />
+        <h2>Enagram</h2>
+      </div>
+      <nav className={`${styles.nav} ${isOpen ? styles.active : ""}`}>
+        <ul className={styles.navList}>
+          <li>Home</li>
+          <li>Map</li>
+        </ul>
+      </nav>
+
+      <div
+        className={`${styles.burger} ${isOpen ? styles.activeBurger : ""}`}
+        onClick={toggleMenu}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      </div>
+       <aside className={styles.sidebar}>
+        <div>
+        <div className={styles.arrows}>
+            <img src={vectors} alt="vectors" />
+        </div>
+       <div className={styles.logo}>
+        <img src={logo} alt='logo' />
+        <h2>Enagram</h2>
+      </div>
+
+      <nav className={styles.navSidebar}>
+    <ul>
+        <li>
+            <img src={check} alt="check" />
+        <a href="#" className={styles.navItem}>მართლმწერი</a>
+        </li>
+        <li>
+             <img src={text} alt="text" />
+        <a href="#" className={styles.navItem}>ტექსტის შედარება</a>
+        </li>
+        <li>
+         <img src={mic} alt="mic" />
+        <a href="#" className={styles.navItem}>ტექსტი</a>
+        </li>
+        <li>
+            <img src={wave} alt="wave" />
+        <a href="#" className={styles.navItem}>ხმა</a>
+        </li>
+        <li>
+            <img src={docs} alt="docs" />
+        <a href="#" className={styles.navItem}>PDF კონვერტაცია</a>
+        </li>
+    </ul>
+      </nav>
+    </div>
+      <div className={styles.user}>
+        <div className={styles.avatar}>
+          {userName.charAt(0).toUpperCase()}
+        </div>
+        <span>{userName}</span>
+      </div>
+    </aside>
+    </header>
+  );
+}
+
+export default Header
