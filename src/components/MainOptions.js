4ed96e32447bf5590975773a4dc47ebed5f7eb25
Davit Nakhutsrishvili <dtnakhutsrishvili@gmail.com>
2026-03-03 22:49:26 +0400

    feat:add mobile version header

---
+++ src/components/MainOptions.js
@@ -0,0 +1,38 @@
+
+import { useState } from 'react';
+import styles from './mainOptions.module.css'
+import plus from '../assets/icons/plus.png'
+
+function MainOptions() {
+      const [isChecked, setIsChecked] = useState(false);
+
+  const handleChange = (e) => {
+    setIsChecked(e.target.checked);
+  };
+  return (
+    <div className={styles.container}>
+    <div className={styles.selectWrapper}>
+    <select className={styles.select} name="language">
+        <option value="georgian">ქართული</option>
+        <option value="english">ინგლისური</option>
+    </select>
+    </div>
+    <div className={styles.checkbox}>
+          <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={handleChange}
+      />
+      <label>
+      ფორმატის შენარჩუნება
+    </label>
+    </div>
+    <div className={styles.btnWrapper}>
+        <button> <img src={plus} alt='plus'/> ახლის გახსნა</button>
+    </div>
+    <hr/>
+    </div>
+  )
+}
+
+export default MainOptions
--- src/pages/TextCompare.jsx
+++ src/pages/TextCompare.jsx
@@ -1,8 +1,10 @@
+import MainOptions from "../components/MainOptions"
 import OptionForm from "../components/OptionForm"
 function TextCompare() {
   return (
     <main>
         <OptionForm/>
+        <MainOptions/>
     </main>
         )
 }
